67f1723b932e195c42200acf82f4314c58bb99c9
marina <londanv@yandex.ru>
2023-05-30 10:03:31 +0700

    start2

--- app/src/main/java/com/example/pr9_koshelevskaya_popova_22106/MainActivity.java
+++ app/src/main/java/com/example/pr9_koshelevskaya_popova_22106/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.pr9_koshelevskaya_popova_22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button5);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button5){
+            startActivity(new Intent(MainActivity.this, MainActivity2.class));
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/pr9_koshelevskaya_popova_22106/MainActivity2.java
+++ app/src/main/java/com/example/pr9_koshelevskaya_popova_22106/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.pr9_koshelevskaya_popova_22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        button = findViewById(R.id.button10);
+        button.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+        Intent browserIntent = new
+        Intent(Intent.ACTION_VIEW, Uri.parse("https://www.android.com"));
+        startActivity(browserIntent);
+    }
+
+
 }
